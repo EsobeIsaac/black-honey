6da84428bcd32ec0f2d2f40e8be6813f71a4a92b
Esobe Isaac <esobeisaac20@gmail.com>
2023-12-30 01:45:13 +0100

    poem block touched

--- components/PoemBlock.js
+++ components/PoemBlock.js
@@ -1,9 +1,10 @@
-import { Box, Container, Grid, Card, CardActions, CardContent, CardMedia, Typography, Button } from "@mui/material";
+import { Box, Container, Grid, Card, CardActions, CardContent, CardMedia, Typography, Button, IconButton } from "@mui/material";
 import Image from "next/image";
 import classes from '@styles/RecentPoems.module.css'
 import Link from "next/link";
 import Loading from "./Loading";
 import ErrorMessage from "./ErrorMessage";
+import { ReadMoreRounded } from "@mui/icons-material";
 
 function RecentPoems({data, title}) {
 
@@ -19,14 +20,14 @@ function RecentPoems({data, title}) {
                     {
                         data.poems && data.poems[0] ? data.poems.map((item, index)=>{
                             return <Grid item key={index} xs={6} sm={6} md={4} lg={4} xl={3}>
-                                <Card sx={{margin: '0px 5px'}}>
-                                    <CardMedia component='img' sx={{height: 250}} alt='image' image={item.image}/>
-                                    <CardContent>
-                                        <Typography variant='subtitle1'>{item.title}</Typography>
+                                <Card sx={{margin: '0px 5px', bgcolor: 'primary'}} className={classes.box}>
+                                    <CardMedia component='img' className={classes.media} sx={{height: 250}} alt='image' image={item.image}/>
+                                    <CardContent bgcolor='#0d1321'>
+                                        <Typography color='primary' textAlign={'center'} variant='subtitle1'>{item.title}</Typography>
                                     </CardContent>
-                                    <CardActions >
-                                    <Link href={`/poems/${item._id}`} style={{width: '100%'}}>
-                                        <Button variant='contained' color={'primary'} size='small' fullWidth>Read</Button>
+                                    <CardActions className={classes.action}>
+                                    <Link href={`/poems/${item._id}`}> 
+                                        <IconButton sx={{backgroundColor: 'primary.main', color: '#fff'}} size="large"><ReadMoreRounded/></IconButton>
                                     </Link>
                                     </CardActions>
                                 </Card>
